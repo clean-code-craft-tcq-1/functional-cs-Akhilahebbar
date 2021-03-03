35ef0afb0310e04c2ecf00867a37b47d40c0f5c3
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-03 08:58:33 +0530

    Update BatteryStatus.cs

--- BatteryStatus.cs
+++ BatteryStatus.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace BatteryManagement
+{
+    internal class BatteryStatus
+    {
+    
+        public static void CheckBatteryCondition(float temperature, float soc, float chargeRate)
+        {
+            BatteryCondition.CheckTemperature(temperature);
+            BatteryCondition.CheckChargeState(soc);
+            BatteryCondition.CheckChargeRate(chargeRate);
+        }
+
+        public static void DisplayBatteryCondition(string attribute, Constants.BatteryStates batteryCondition)
+        {
+            Console.WriteLine($"Battery {attribute} is {batteryCondition}");
+        }
+    }
+}
